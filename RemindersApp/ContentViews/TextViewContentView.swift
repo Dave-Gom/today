45fba5483f8b3de32fdb11f820b82e1fc97dca28
Dave Gomez <davegomez426@gmail.com>
2025-10-17 17:45:21 -0300

    Update changes of notes

--- RemindersApp/ContentViews/TextViewContentView.swift
+++ RemindersApp/ContentViews/TextViewContentView.swift
@@ -10,6 +10,7 @@ import UIKit
 class TextViewContentView: UIView, UIContentView {
     struct Configuration: UIContentConfiguration {
         var text: String? = ""
+        var onChange: (String?) -> Void = { _ in }
         
         func makeContentView() -> UIView & UIContentView {
             return TextViewContentView(self)
@@ -19,7 +20,7 @@ class TextViewContentView: UIView, UIContentView {
     let textView = UITextView()
     var configuration: UIContentConfiguration {
         didSet{
-            
+            configure(configuration: configuration)
         }
     }
     
@@ -32,6 +33,7 @@ class TextViewContentView: UIView, UIContentView {
         super.init(frame: .zero)
         addPinnedSubview(textView, height: 200)
         textView.backgroundColor = nil
+        textView.delegate = self
         textView.font = UIFont.preferredFont(forTextStyle: .body)
         
     }
@@ -44,11 +46,17 @@ class TextViewContentView: UIView, UIContentView {
         guard let configuration = configuration as? Configuration else { return }
         textView.text = configuration.text
     }
-    
 }
 
 extension UICollectionViewListCell {
     func textViewConfiguration() -> TextViewContentView.Configuration {
         TextViewContentView.Configuration()
+    }
+}
+
+extension TextViewContentView: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        guard let configuration = configuration as? TextViewContentView.Configuration else { return }
+        configuration.onChange(textView.text)
     }
 }

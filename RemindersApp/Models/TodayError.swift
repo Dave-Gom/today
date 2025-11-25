c970b70e06279b100ddc61651c47cd6f7ebb703d
David Gómez <davegomez426@gmail.com>
2025-11-24 21:21:22 -0300

    Loading reminders and pasing them to Reminers class

--- RemindersApp/Models/TodayError.swift
+++ RemindersApp/Models/TodayError.swift
@@ -12,6 +12,7 @@ enum TodayError: LocalizedError {
     case accessRestricted
     case failedReadingReminders
     case reminderHasNoDueDate
+    case failedReadingCalendarItem
     case unknown
     
     var errorDescription: String? {
@@ -24,6 +25,8 @@ enum TodayError: LocalizedError {
             return NSLocalizedString("A reminder has no due date", comment: "reminder has no due date error description")
         case .accessRestricted:
             return NSLocalizedString("This device does not allow access to reminders", comment: "acces restricted error description")
+        case .failedReadingCalendarItem:
+            return NSLocalizedString("Failed to read calendar item.", comment: "failed  reading calendar item error description")
         case .unknown:
             return NSLocalizedString("An unknown erro occured", comment: "unknown error description")
         }
